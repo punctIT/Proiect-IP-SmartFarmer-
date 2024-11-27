31d290097c540b329f804bc59744d521a7b325d8
punctIT <teodorivan2005@gmail.com>
2024-11-27 19:29:59 +0200

    Initial commit

---
+++ Home/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <graphics.h>
+#include <winbgim.h>
+
+int main()
+{
+// int gd = DETECT, gm;
+// initgraph(&gd, &gm, "");
+  initwindow(800,600);
+  bar(44,200,200,200);
+  getch(); return 0;
+}
