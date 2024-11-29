6d563ff2eba3ede6517f8e771330fcf13ee6e263
punctIT <teodorivan2005@gmail.com>
2024-11-29 20:46:02 +0200

    update1

--- Home/src/main.cpp
+++ Home/src/main.cpp
@@ -1,12 +1,88 @@
 #include <iostream>
-#include <graphics.h>
+#include <cstring>
 #include <winbgim.h>
+#include <stdlib.h>
+#include <fstream>
+using namespace std;
 
+
+
+char GameBoard[7][9];
+int length=9,width=7;
+
+struct GamePieces{
+    int x,y;
+    struct{
+       int n;
+       pair <int,int> coordonate[7]; //coordonatele in maticea de marcare, first- x , second y
+       char symbol='5';
+    }GB;
+    
+}Fence;
+void readFence(GamePieces &fence,string FileName)
+{
+    ifstream fin(FileName);
+    fin>>fence.GB.n;
+    for(int i=1;i<=fence.GB.n;i++)
+        fin>>fence.GB.coordonate[i].first>>fence.GB.coordonate[i].second;
+    
+}
+bool VerifyFencePosition(GamePieces fence,int x,int y)
+{
+    for (int i = 1; i <= fence.GB.n; i++)
+        if (strchr("01234", GameBoard[fence.GB.coordonate[i].first+y][fence.GB.coordonate[i].second+x])==NULL)
+            return false;
+    return true;
+}
+void addFance(GamePieces fence)
+{
+    if(!VerifyFencePosition(fence,0,0)){
+        cout<<"erroare"<<endl;
+        return;
+    }
+    for (int i = 1; i <= fence.GB.n; i++)
+        GameBoard[fence.GB.coordonate[i].first][fence.GB.coordonate[i].second]++;
+}
+void moveFence(GamePieces& fence,int x,int y)//x coloana , y linia
+{
+    if(!VerifyFencePosition(fence,x,y)){
+        cout<<"erroare1"<<endl;
+        return;
+    }
+    for (int i = 1; i <= fence.GB.n; i++){
+        GameBoard[fence.GB.coordonate[i].first][fence.GB.coordonate[i].second]--;
+        fence.GB.coordonate[i].first+=y;
+        fence.GB.coordonate[i].second+=x;
+    }
+    addFance(fence);
+   
+}
+void DrawGameBoard()
+{
+    for(int i=0;i<width;i++)
+    {
+        for(int q=0;q<length;q++)
+            cout<<GameBoard[i][q]<<" ";
+        cout<<endl;
+    }
+}
+void readGameBoard()
+{
+    ifstream fin("GameBoards/GameBoard.txt");
+    for(int i=0;i<width;i++)
+        for(int q=0;q<length;q++)
+            fin>>GameBoard[i][q];
+}
 int main()
 {
-// int gd = DETECT, gm;
-// initgraph(&gd, &gm, "");
-  initwindow(800,600);
-  bar(1,10,200,200);
-  getch(); return 0;
+    readGameBoard();
+    readFence(Fence,"GameBoards/Piesa1.txt");
+    readFence(fence2,"GameBoards/Piesa1.txt");
+    redraw:
+    DrawGameBoard();
+    cout<<endl;
+    moveFence(Fence,1,0);
+    moveFence(Fence,1,0);
+    moveFence(Fence,-1,2);
+    DrawGameBoard();
 }
