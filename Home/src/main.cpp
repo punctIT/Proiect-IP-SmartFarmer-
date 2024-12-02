68d71764faa3f6e68724cb333204cbca1e63670e
punctIT <teodorivan2005@gmail.com>
2024-12-02 22:10:13 +0200

    Piesa care se misca, DE REVAZUT CA E DuBIOASA

--- Home/src/main.cpp
+++ Home/src/main.cpp
@@ -12,6 +12,7 @@ int length = 9, width = 7;
 struct GamePieces
 {
     int x, y;
+    POINT UpLeft, DownRight;
     int GB[7][9];
 
 } Fence;
@@ -37,7 +38,7 @@ void addFance(GamePieces fence)
 {
     if (!VerifyFencePosition(fence, 0, 0))
     {
-        cout << "erroare";
+         cout<<"erroare";
         return;
     }
     for (int i = 0; i < width; i++)
@@ -48,7 +49,7 @@ void moveFence(GamePieces &fence, int x, int y) // x coloana , y linia(indicele 
 {
     if (!VerifyFencePosition(fence, x, y))
     {
-        cout << "erroare1" << endl;
+        cout << "erroare ceva "<<x<<" "<<y << endl;
         return;
     }
     GamePieces auxMatrix = {0};
@@ -94,9 +95,9 @@ void DrawInBoardGame(int x, int y, int color)
 }
 void DrawBoardGame()
 {
-    
+
     gbSideLength = 80;
-    gbWidth = gbSideLength*length;
+    gbWidth = gbSideLength * length;
     gbHeight = 400;
     UpBorder = 50;
     LeftBorder = 50;
@@ -112,9 +113,10 @@ void DrawBoardGame()
                 DrawInBoardGame(q + 1, i + 1, GameBoard[i][q] - '0');
 }
 
-void drawFence(GamePieces fence, int x, int y, int side)
+void drawFence(GamePieces &f, int x, int y, int side)
 {
     int ZeroLineNumber = 0, ZeroColumnNumber = 0;
+    GamePieces fence = f;
     for (int i = 0; i < width; i++)
     {
         bool ok = true;
@@ -149,9 +151,9 @@ void drawFence(GamePieces fence, int x, int y, int side)
     for (int i = width - ZeroLineNumber; i < width; i++)
         for (int q = 0; q < length; q++)
             fence.GB[i][q] = 0;
-    for(int i=length-ZeroColumnNumber;i<length;i++)
-        for(int q=0;q<width;q++)
-            fence.GB[q][i]=0;
+    for (int i = length - ZeroColumnNumber; i < length; i++)
+        for (int q = 0; q < width; q++)
+            fence.GB[q][i] = 0;
     /*
     ofstream fout("GameBoards/coordonate.txt", ios_base::app);
     for(int i=0;i<width;i++)
@@ -161,36 +163,88 @@ void drawFence(GamePieces fence, int x, int y, int side)
         fout<<endl;
     }
     */
-    for (int i = 0; i < width; i++){
+    for (int i = 0; i < width; i++)
+    {
         //  ofstream fout("GameBoards/coordonate.txt", ios_base::app);
-        for (int q = 0; q < length; q++){
+        for (int q = 0; q < length; q++)
+        {
             //  fout<<fence.GB[i][q]<<" ";
             if (fence.GB[i][q])
             {
                 x += side * (q);
                 y += side * (i);
+               
+              
                 rectangle(x, y, x + side, y + side);
+                 setfillstyle(SOLID_FILL, BLUE);
+                  floodfill(x+1,y+1,15);
+                 
                 y -= side * (i);
                 x -= side * (q);
             }
         }
     }
+    f.UpLeft.x = x;
+    f.UpLeft.y = y;
+    for (int i = 0; i < width; i++)
+        for (int q = 0; q < length; q++)
+        if(fence.GB[i][q])
+        {
+            f.DownRight.x = f.UpLeft.x + side * q+side;
+            f.DownRight.y = f.UpLeft.y + side * i+side;
+        }
+    /*
+    ofstream fout("GameBoards/coordonate.txt", ios_base::app);
+    fout<<f.UpLeft.x<<" "<<f.UpLeft.y<<endl;
+    fout<<f.DownRight.x<<" "<<f.DownRight.y<<endl;
+    fout<<endl;
+    */
 }
-
-POINT MouseCoordonates() // RETURNEAZA coodonatele in matrice, unde se afla mouseul
+int page = 0;
+POINT MouseDraggingPiece(GamePieces& Fence) 
 {
     POINT GB;
+    bool dragging;
+    setactivepage(page);
+    setvisualpage(1 - page);
+    setfillstyle(SOLID_FILL, BLACK);
+    bar(0, 0, getmaxx(), getmaxy());
     if (ismouseclick(WM_LBUTTONDOWN))
     {
-        Beep(250, 600);
+       
         clearmouseclick(WM_LBUTTONDOWN);
-        int x = mousex();
-        int y = mousey();
-        GB.y = (x - LeftBorder) / gbSideLength;
-        GB.x = (y - UpBorder) / gbSideLength;
+        POINT mouse;
+        GetCursorPos(&mouse);
+        
+        if (mouse.x >= Fence.UpLeft.x && mouse.x <= Fence.DownRight.x && mouse.y >= Fence.UpLeft.y && mouse.y <=Fence.DownRight.y) {
+                dragging = 1; // Începe drag-and-drop
+        }
+        GB.y = (mouse.x - LeftBorder) / gbSideLength;
+        GB.x = (mouse.y - UpBorder) / gbSideLength;
         ofstream fout("GameBoards/coordonate.txt", ios_base::app);
         fout << GB.x << " " << GB.y << endl;
     }
+    if(dragging)
+    {
+          POINT mouse;
+          GetCursorPos(&mouse);
+          Fence.UpLeft.x=mouse.x-40;
+          Fence.UpLeft.y=mouse.y-40;
+          if (ismouseclick(WM_RBUTTONDOWN)){
+                dragging = 0; // Termină drag-and-drop
+                clearmouseclick(WM_RBUTTONDOWN);
+                 GB.y = (mouse.x - LeftBorder) / gbSideLength;
+                GB.x = (mouse.y - UpBorder) / gbSideLength;
+                ofstream fout("GameBoards/coordonate.txt", ios_base::app);
+                fout << GB.x << " " << GB.y << endl;
+                
+          }
+    }
+    DrawBoardGame();
+    drawFence(Fence,Fence.UpLeft.x,Fence.UpLeft.y,80);
+
+    page = 1 - page;
+    delay(10); 
     return GB;
 }
 int main()
@@ -199,15 +253,18 @@ int main()
     readGameBoard("GameBoards/GameBoard.txt");
     readFence(Fence, "GameBoards/Piesa3.txt");
     addFance(Fence);
-    moveFence(Fence, 2, 2);
+    moveFence(Fence,2,0);
 
     initwindow(1400, 700);
     DrawBoardGame();
-   
-    drawFence(Fence,LeftBorder+gbWidth+LeftBorder , UpBorder, gbSideLength);
+    
+    
+
+    drawFence(Fence, LeftBorder + gbWidth + LeftBorder, UpBorder, gbSideLength);
+    rectangle(Fence.UpLeft.x,Fence.UpLeft.y,Fence.DownRight.x, Fence.DownRight.y);
     while (true)
     {
-        MouseCoordonates();
+        MouseDraggingPiece(Fence);
     }
 
     getch();
